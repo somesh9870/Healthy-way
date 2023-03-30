02b8cb6517973162305a5ff6f4153b3d04584020
MalikRehan-M <malikrehan.meeranaik@gmail.com>
2023-03-30 22:53:54 +0530

    Day-3 mid section & bottom section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,16 +4,18 @@ import {
   Flex,
   Grid,
   GridItem,
+  HStack,
   Image,
   Link,
   Text,
+  VStack,
 } from "@chakra-ui/react";
 import React from "react";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-   <div>
+    <div>
       <Navbar />
       <Box
         bgImage={"https://cdn1.cronometer.com/webflow/dot-pattern.svg"}
@@ -75,7 +77,7 @@ const Home = () => {
           </GridItem>
         </Grid>
       </Box>
-      <Box w={"100%"} mt="20px">
+      <Box w={"100%"} mt="20px" bgColor={"#f0f2fa"} pt={"30px"} pb={"20px"}>
         <Text fontSize={"xl"} textAlign="center">
           As Seen On
         </Text>
@@ -89,20 +91,28 @@ const Home = () => {
           gap={["30px", "25px", "0", "0"]}
         >
           <Box w={["70%", "70%", "30%", "20%"]}>
-            <Image src="https://cdn1.cronometer.com/webflow/media-mention-2.svg" alt="media2" w={"90%"} />
+            <Image
+              src="https://cdn1.cronometer.com/webflow/media-mention-2.svg"
+              alt="media2"
+              w={"90%"}
+            />
           </Box>
           <Box w={["70%", "70%", "30%", "25%"]}>
-            <Image src="https://cdn1.cronometer.com/webflow/media-mention-1.svg" alt="media1"  w={"80%"} />
+            <Image
+              src="https://cdn1.cronometer.com/webflow/media-mention-1.svg"
+              alt="media1"
+              w={"80%"}
+            />
           </Box>
           <Box w={["70%", "70%", "30%", "25%"]}>
-            <Image src="https://cdn1.cronometer.com/webflow/media-mention-3.svg" alt="media3"  w={"80%"}/>
+            <Image
+              src="https://cdn1.cronometer.com/webflow/media-mention-3.svg"
+              alt="media3"
+              w={"80%"}
+            />
           </Box>
         </Flex>
-        <Text
-          fontSize={"xl"}
-          textAlign="center"
-          mt="35px"
-        >
+        <Text fontSize={"xl"} textAlign="center" mt="35px">
           Available on Web, iOS and Android
         </Text>
         <Flex
@@ -115,7 +125,11 @@ const Home = () => {
           gap={["20px", "20px", "none"]}
         >
           <Link to="">
-            <Image src={"https://cdn1.cronometer.com/webflow/ios-icon.svg"} alt="ios" w={["80%", "80%", "80%"]} />
+            <Image
+              src={"https://cdn1.cronometer.com/webflow/ios-icon.svg"}
+              alt="ios"
+              w={["80%", "80%", "80%"]}
+            />
           </Link>
           <Link to="">
             <Image
@@ -125,6 +139,285 @@ const Home = () => {
             />
           </Link>
         </Flex>
+      </Box>
+      <Box mt={"150px"} textAlign="center">
+        <Text fontSize={"4xl"} fontWeight="800">
+          Develop healthy habits
+        </Text>
+        <Text fontSize={"xl"}>
+          Count your calories, ensure you're meeting nutrient <br />
+          targets, and see your progress over time.
+        </Text>
+        <Grid
+          pt={"80px"}
+          w={"67%"}
+          h="80%"
+          margin={"auto"}
+          gridTemplateColumns={{
+            base: "repeat(1,1fr)",
+            md: "repeat(2,1fr)",
+            lg: "repeat(3,1fr)",
+          }}
+          gap={20}
+          align="center"
+        >
+          <GridItem>
+            <Image
+              h={"70px"}
+              src="https://cdn1.cronometer.com/webflow/cronometer-features-11.svg"
+            ></Image>
+            <Text fontSize={"2xl"} fontWeight="500">
+              Log meals, exercise and <br />
+              health metrics
+            </Text>
+            <Text>
+              Plus, you can create custom foods, recipes, exercises and metrics.
+            </Text>
+          </GridItem>
+          <GridItem>
+            {" "}
+            <Image
+              h={"70px"}
+              src="https://cdn1.cronometer.com/webflow/cronometer-features-14.svg"
+            ></Image>
+            <Text fontSize={"2xl"} fontWeight="500">
+              Get valuable health reports <br />
+              and charts
+            </Text>
+            <Text>Learn how nutrients and metrics correlate over time</Text>
+          </GridItem>
+
+          <GridItem>
+            {" "}
+            <Image
+              h={"70px"}
+              src="https://cdn1.cronometer.com/webflow/cronometer-features-13.svg"
+            ></Image>
+            <Text fontSize={"2xl"} fontWeight="500">
+              Get valuable health reports <br />
+              and charts
+            </Text>
+            <Text>Learn how nutrients and metrics correlate over time.</Text>
+          </GridItem>
+          <GridItem>
+            {" "}
+            <Image
+              h={"70px"}
+              src="https://cdn1.cronometer.com/webflow/cronometer-features-16.svg"
+            ></Image>
+            <Text fontSize={"2xl"} fontWeight="500">
+              Custom diet settings
+            </Text>
+            <Text>
+              Set weight, macro and nutrient targets to meet your goals.
+            </Text>
+          </GridItem>
+          <GridItem>
+            {" "}
+            <Image
+              h={"70px"}
+              src="https://cdn1.cronometer.com/webflow/cronometer-features-15.svg"
+            ></Image>
+            <Text fontSize={"2xl"} fontWeight="500">
+              Fasting timer
+            </Text>
+            <Text>
+              Track your intermittent fasts and see their effect over time.
+            </Text>
+          </GridItem>
+          <GridItem>
+            {" "}
+            <Image
+              h={"70px"}
+              src="https://cdn1.cronometer.com/webflow/cronometer-features-12.svg"
+            ></Image>
+            <Text fontSize={"2xl"} fontWeight="500">
+              Diet support
+            </Text>
+            <Text>
+              Whether you’re Keto, Vegan, or on one recommended by your doctor.
+            </Text>
+          </GridItem>
+        </Grid>
+        <Text mt={"180px"} fontSize={"4xl"} fontWeight="800">
+          Discover your nutrition
+        </Text>
+        <Text fontSize={"xl"}>
+          Cronometer encourages you to not just count your calories but to{" "}
+          <br /> focus on your nutrition as a whole.
+        </Text>
+        <Grid
+          pt={"50px"}
+          w={"67%"}
+          margin={"auto"}
+          gridTemplateColumns={{
+            base: "repeat(1,1fr)",
+            md: "repeat(2,1fr)",
+            lg: "repeat(2,1fr)",
+          }}
+          gap={20}
+          align="center"
+        >
+          <GridItem>
+            <Image
+              h={"100%"}
+              src="https://cdn1.cronometer.com/webflow/crono-hero-img-22x-1.png"
+            ></Image>
+          </GridItem>
+          <GridItem mt={"100px"}>
+            <VStack h={"85%"} justifyContent={"space-between"}>
+              <HStack align="start" gap={"12px"}>
+                <Image
+                  w={"37px"}
+                  src="https://cdn1.cronometer.com/webflow/checkmark.svg"
+                ></Image>
+                <Box textAlign="start">
+                  <Text fontSize={"2xl"} fontWeight="500">
+                    Over 6 million users
+                  </Text>
+                  <Text fontSize={"18px"}>
+                    Join the community to get tips and inspiration from other
+                    users on our forums and Facebook group.
+                  </Text>
+                </Box>
+              </HStack>
+              <HStack align="start" gap={"12px"}>
+                <Image
+                  w={"37px"}
+                  src="https://cdn1.cronometer.com/webflow/checkmark.svg"
+                ></Image>
+                <Box textAlign="start">
+                  <Text fontSize={"2xl"} fontWeight="500">
+                    Accurate nutrition data
+                  </Text>
+                  <Text fontSize={"18px"}>
+                    Be confident that the food you log has the correct nutrition
+                    data. We verify every food submission for accuracy.
+                  </Text>
+                </Box>
+              </HStack>
+              <HStack align="start" gap={"12px"}>
+                <Image
+                  w={"37px"}
+                  src="https://cdn1.cronometer.com/webflow/checkmark.svg"
+                ></Image>
+                <Box textAlign="start">
+                  <Text fontSize={"2xl"} fontWeight="500">
+                    Data privacy & security
+                  </Text>
+                  <Text fontSize={"18px"}>
+                    We don't sell your account data to third parties and take
+                    the security of our users' accounts seriously.
+                  </Text>
+                </Box>
+              </HStack>
+            </VStack>
+          </GridItem>
+        </Grid>
+        <Box h={"230px"} mt="200px" bgColor={"#262a3b"} pt="35px">
+          <Text color={"white"} fontSize={"21px"}>
+            Find out if you're getting the vitamins and minerals you need <br />{" "}
+            with our food diary app.
+          </Text>
+          <Button
+            mt={"30px"}
+            href={"#"}
+            fontSize={"2xl"}
+            p={"30px 45px 30px 45px"}
+            bg={"#44d07b"}
+            _hover={{
+              bg: "#44d07b",
+              boxShadow: "dark-lg",
+            }}
+          >
+            Sign Up - It's Free!
+          </Button>
+        </Box>
+        <Text mt={"180px"} fontSize={"4xl"} fontWeight="800">
+          Sync with apps and devices
+        </Text>
+        <Image
+          m="auto"
+          mt={"30px"}
+          w="50%"
+          src="https://cdn1.cronometer.com/webflow/cronometer-devices-3-p-1600.png"
+        ></Image>
+        <Box h={"85vh"} bgColor="#f0f2fa">
+          <Text mt={"150px"} fontSize={"4xl"} fontWeight="800">
+            Healthy Way reviews
+          </Text>
+          <Box textAlign={"start"}>
+            <HStack
+              bgColor={"white"}
+              w="57%"
+              m="auto"
+              p="30px"
+              borderRadius={"6px"}
+              boxShadow="sm"
+              justifyContent={"space-around"}
+            >
+              <Box>
+                <Text color={"#005c5c"} fontSize="xl" fontWeight="700">
+                  A great all-in-one health app!
+                </Text>
+                <Image
+                  m={"auto"}
+                  mt="20px"
+                  w="150px"
+                  src="https://cdn1.cronometer.com/webflow/ios-icon.svg"
+                ></Image>
+              </Box>
+              <Box>
+                <Text>
+                  After leaving SparkPeople, I came here. I love that it is
+                  Canadian,
+                  <br /> includes fasting, can import recipes, gives me macro
+                  details, and <br /> more. This is my daily tracking app.
+                </Text>
+                <Text color={"#005c5c"} mt="20px" fontSize={"xl"}>
+                  iOS Review
+                </Text>
+              </Box>
+            </HStack>
+            <HStack
+              bgColor={"white"}
+              w="57%"
+              m="auto"
+              mt="30px"
+              p="30px"
+              borderRadius={"6px"}
+              boxShadow="sm"
+              justifyContent={"space-around"}
+            >
+              <Box>
+                <Text>
+                  Awesome. Getting direct feedback when you track your food{" "}
+                  <br />
+                  really opens your eyes to what you're putting in your body.{" "}
+                  <br />
+                  The modularity and ability to track macros, micros, and
+                  calories in - <br /> calories out makes this an invaluable
+                  tool for any nutritional goal.
+                </Text>
+                <Text color={"#005c5c"} mt="20px" fontSize={"xl"}>
+                  Android Review
+                </Text>
+              </Box>
+              <Box>
+                <Text color={"#005c5c"} fontSize="xl" fontWeight="700">
+                  Whether that's bodybuilding, weight <br /> loss, endurance training,
+                  or just <br /> maintenance – everyone should be <br /> using Cronometer.
+                </Text>
+                <Image
+                  m={"auto"}
+                  mt="20px"
+                  w="150px"
+                  src="https://cdn1.cronometer.com/webflow/android-icon.svg"
+                ></Image>
+              </Box>
+            </HStack>
+          </Box>
+        </Box>
       </Box>
     </div>
   );
