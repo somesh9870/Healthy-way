48bae3a538cd60b41ebddbaefa07b530a6ad2b97
Ojas <ojasbendale18200@gmail.com>
2023-04-02 20:14:11 +0530

    all completed

--- frontend/src/pages/Admin/AdminHome.jsx
+++ frontend/src/pages/Admin/AdminHome.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import SidebarWithNav from "../../components/SidebarWithNav";
+import UserStats from "../../components/UserStats";
+import { Heading } from "@chakra-ui/react";
 
 const AdminHome = () => {
   return (
     <div>
       <SidebarWithNav />
+      <div style={{ margin: "auto", marginLeft: "20%" }}>
+        <Heading textAlign={"center"} mt={"50px"} mb={"50px"}>
+          DashBoard
+        </Heading>
+        <UserStats />
+      </div>
     </div>
   );
 };
